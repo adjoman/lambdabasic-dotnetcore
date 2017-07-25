696fad051ab1fa599e782c5162bff472c650794d
adjoman <calvin@kairuconsulting.com>
2017-07-24 23:09:26 -0400

    Initial check-in of module LinqBasicDotNetCore

---
+++ Start.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Linq;
+
+namespace LinqBasicDotNetCore
+{
+    public class Start
+    {
+        static void Main(string[] args)
+        {
+			// Language Integrated Query Example (LINQ)
+			Console.WriteLine("Demonstrating Basic Linq");
+
+			int[] listOfNumbers = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
+
+			// get all even numbers
+			var result = from number in listOfNumbers
+						 where (number % 2) == 0
+						 select number;
+
+			foreach (var r in result)
+			{
+				Console.WriteLine(r.ToString());
+			}
+
+			Console.ReadLine();
+        }
+
+    }
+}
